af3e27fe938d3a8d8433af89981b9e2333dd23d3
Ion Bunescu <ion140290@gmail.com>
2018-09-27 11:15:49 +0300

    sortable

--- frontend/core/views/overview-grid.php
+++ frontend/core/views/overview-grid.php
@@ -29,29 +29,31 @@ $price_color = get_option("at_price_color");
 
                         <div class="selectorB4uAuto">
 
-                            <select name="sort" id="sortSelect" class="selectCustom">
+                            <select name="orderBy" id="sortSelect" class="selectCustom">
+                                <option value>Sorteren op...</option>
+
                                 <option
-                                        value="catalog_price" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'catalog_price' ? 'selected' : '' ?>>
+                                    value="catalog_price" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'catalog_price' ? 'selected' : '' ?>>
                                     Prijs, laag naar hoog
                                 </option>
                                 <option
-                                        value="catalog_price_desc" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'catalog_price_desc' ? 'selected' : '' ?>>
+                                    value="catalog_price_desc" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'catalog_price_desc' ? 'selected' : '' ?>>
                                     Prijs, hoog naar laag
                                 </option>
                                 <option
-                                        value="brands.title" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'brands.title' ? 'selected' : '' ?>>
+                                    value="brand" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'brand' ? 'selected' : '' ?>>
                                     Merk, Model, Uitvoering
                                 </option>
                                 <option
-                                        value="mileages.mileage" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'mileages.mileage' ? 'selected' : '' ?>>
+                                    value="mileage" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'mileage' ? 'selected' : '' ?>>
                                     Kilometerstand
                                 </option>
                                 <option
-                                        value="datumGeplaatst" <?php echo isset($_GET['sort']) && $_GET['sort'] == 'datumGeplaatst' ? 'selected' : '' ?>>
+                                    value="date_posted" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'date_posted' ? 'selected' : '' ?>>
                                     Publicatiedatum
                                 </option>
                                 <option
-                                        value="construction_year" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'construction_year' ? 'selected' : '' ?>>
+                                    value="construction_year" <?php echo isset($_GET['orderBy']) && $_GET['orderBy'] == 'construction_year' ? 'selected' : '' ?>>
                                     Bouwjaar
                                 </option>
 
