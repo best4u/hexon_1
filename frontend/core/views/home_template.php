f03e6e42b7b48abaf9c9a66d8d92c6dcb7ba7dc7
Ion Bunescu <ion140290@gmail.com>
2018-10-02 13:35:34 +0300

    thumbnails on home cars

--- frontend/core/views/home_template.php
+++ frontend/core/views/home_template.php
@@ -12,8 +12,8 @@
                 <div class="imgBlock">
                     <div class="imgTable">
                         <div class="imgTableCell">
-                          <img src="<?php if ($car->images->{'image-1'}->thumbs->{'320_240'} != '') {
-                                    echo $car->images->{'image-1'}->thumbs->{'320_240'};
+                          <img src="<?php if ($car->images->{'image-1'}->thumbs->{'320_'} != '') {
+                                    echo $car->images->{'image-1'}->thumbs->{'320_'};
                                     } else {
                                     echo "https://www.autotrack.nl//vassets/images/ae72c25fe141159ebb00884804e7f9c8-geen-afbeelding-320x240.png";
                                     } ?>" 
